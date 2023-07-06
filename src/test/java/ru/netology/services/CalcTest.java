917fd90838fb5ea53a0cd38e6af74a01f01d22da
Artem Smaragdov <artem_sm@mail.ru>
2023-07-07 00:26:25 +0400

    Initial commit

--- src/test/java/ru/netology/services/CalcTest.java
+++ src/test/java/ru/netology/services/CalcTest.java
@@ -3,16 +3,18 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class CalcTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "3, 10000, 3000, 20000 ",
-            "2, 100000, 60000, 150000"
-    })
+    //@CsvSource({
+      //      "3, 10000, 3000, 20000 ",
+        //    "2, 100000, 60000, 150000"
+    //})
+    @CsvFileSource(files="src/test/resources/calc.csv")
     public void test(int expected, int income, int expenses, int threshold) {
         Calc service = new Calc();
         int actual = service.calculate(income, expenses, threshold);
